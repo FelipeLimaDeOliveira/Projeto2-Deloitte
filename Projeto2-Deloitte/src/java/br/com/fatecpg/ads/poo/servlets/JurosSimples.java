490392ffba4c11cadcd54ccfec71dfdf6782210c
acbertini <acbertini@gmail.com>
2018-03-25 10:46:30 -0300

    exclusão webservice e alteração texto

--- Projeto2-Deloitte/src/java/br/com/fatecpg/ads/poo/servlets/JurosSimples.java
+++ Projeto2-Deloitte/src/java/br/com/fatecpg/ads/poo/servlets/JurosSimples.java
@@ -63,7 +63,7 @@ public class JurosSimples extends HttpServlet {
             out.println("<table>");
             out.println("<tr><td>Capital:</td><td><input min='0' step='0.01' name='c' required/></td></tr>");
             out.println("<tr><td>Taxa de juros mensal (%):</td><td><input type='number' min='0' step='any' name='i' required/></td></tr>");
-            out.println("<tr><td>PerÃ­odo (meses):</td><td><input type='number' step='1' min='0' name='n' required'/></td></tr>");
+            out.println("<tr><td>Período (meses):</td><td><input type='number' step='1' min='0' name='n' required'/></td></tr>");
             out.println("<tr><td colspan='2' align='center'><input type='submit' value='Calcular'/></td></tr>");
             out.println("</table>");
             out.println("</form>");
